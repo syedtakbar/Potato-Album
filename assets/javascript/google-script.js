50af42a71a1c856716416b7fd28d676db05e32db
Syed T. Akbar <sakbar01@icloud.com>
2019-08-20 18:15:59 -0500

    completed the album creation api development

--- assets/javascript/google-script.js
+++ assets/javascript/google-script.js
@@ -1,8 +1,4 @@
 
-  window.onload = function(){
-    GoogleObj.init();
-  };
-
   const Logger  = {
          
       resultDiv: document.getElementById("div-result"),
@@ -40,13 +36,16 @@
   };
 
   const GoogleObj = {
+    googleObj: this,
     albumObj : {},
     images : ["./assets/images/crystal-1.jpg", "./assets/images/crystal-2.jpg", "./assets/images/crystal-3.jpg", "./assets/images/crystal-4.jpg"],
     authButton: document.getElementById("log-in"),
     createAlbumButton: document.getElementById("create-album"),
     getPicButton: document.getElementById("get-pic"),
     uploadPicButton: document.getElementById("upload-pic"),
-  
+    // selectFilesButton: document.getElementById("get-files"),
+    selectFileButton: document.getElementById("get-file"),
+    
     albumInput: document.getElementById("album-input"),
 
     picDiv: document.getElementById("div-pic"),
@@ -75,6 +74,7 @@
     }, 
 
     authenticate: function () {      
+      this.init();
       return gapi.auth2.getAuthInstance()
       .signIn({scope: this.SignInScope })     
       .then(Logger.logResult,Logger.logError);
@@ -83,7 +83,6 @@
 
 
     loadClient: function (APIKey, clientURL) {
-      //this.init();
       gapi.client.setApiKey(APIKey);
       return gapi.client.load(clientURL)      
       .then(Logger.logResult,Logger.logError);
@@ -127,6 +126,60 @@
       return gapi.client.photoslibrary.albums.create(albumObj)    
         .then(Logger.logAPIResult,Logger.logError); 
     },
+
+    uploadImage : function (imageFile) {
+        
+        const url = "https://photoslibrary.googleapis.com/v1/uploads";
+
+          return fetch(url, {
+              method: 'POST', // *GET, POST, PUT, DELETE, etc.
+              mode: 'cors', // no-cors, cors, *same-origin
+              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+              credentials: 'same-origin', // include, *same-origin, omit
+              headers: {
+                  'Content-Type': 'application/octet-stream',
+                  'X-Goog-Upload-File-Name': imageFile,
+                  'X-Goog-Upload-Protocol': "raw",                  
+              },
+              redirect: 'follow', // manual, *follow, error
+              referrer: 'no-referrer', // no-referrer, *client
+              body: JSON.stringify(data), // body data type must match "Content-Type" header
+          })
+          .then(response => response.json()); // parses JSON response into native JavaScript objects 
+    },
+
+    selectFiles: function (evt) {
+      const files = evt.target.files;       
+      const output = [];
+      for (let i = 0, f; f = files[i]; i++) {
+        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                    f.size, ' bytes, last modified: ',
+                    f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                    '</li>');
+      }
+      document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';      
+    },
+
+    selectFile: function (evt, callback) {          
+      const file = evt.target.files[0];       
+      let output = "";
+      output = "<strong>" + escape(file.name) + "</strong> ( " + (file.type || "n/a" ) + " ) - " +
+                    file.size + " bytes, last modified: " +
+                    (file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : "n/a");
+      
+      document.getElementById('list').innerHTML = output;     
+      callback(file.name);      
+    },
+
+    loadImage: function (imageFile) {         
+      GoogleObj.picDiv.innerHTML = "";
+      const img = document.createElement("img");  
+      const file =   GoogleObj.images[GoogleObj.getRandomInt(0,3)];
+      img.src = imageFile;    
+      img.alt = "missing file:" + file;
+      img.style = "width: 200px; height:200px;";
+      GoogleObj.picDiv.appendChild(img);
+    }
   };
 
   GoogleObj.authButton.addEventListener("click", function () {                    
@@ -139,13 +192,9 @@
     GoogleObj.albumInput.value = "";
   });
 
-  GoogleObj.getPicButton.addEventListener("click", function () {     
-    const img = document.createElement("img");  
+  GoogleObj.getPicButton.addEventListener("click", function () {  
     const file =   GoogleObj.images[GoogleObj.getRandomInt(0,3)];
-    img.src = file;    
-    img.alt = "missing file:" + file;
-    img.style = "width: 200px; height:200px;";
-    GoogleObj.picDiv.appendChild(img);
+    GoogleObj.loadImage(file);    
   });
 
   GoogleObj.uploadPicButton.addEventListener("click", function () {
@@ -158,6 +207,11 @@
     GoogleObj.uploadPicture(GoogleObj.albumObj.id, GoogleObj.picDiv.getElementsByTagName("img")[0].src);
     
   });
+
+  GoogleObj.selectFileButton.addEventListener("change", 
+    function (event) {
+      GoogleObj.selectFile(event, GoogleObj.loadImage);    
+    });
 
 
 
